292a65d26134cf7a7037598616587bb1ccb3b7f6
chee-yew <wongcheeyew1@gmail.com>
2026-03-30 16:10:59 +0800

    Add Clerk authentication to frontend

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { ClerkProvider } from '@clerk/clerk-react'
+
+const clerkPubKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!clerkPubKey) {
+  throw new Error('Clerk publishable key is not defined. Please set VITE_CLERK_PUBLISHABLE_KEY in your environment variables.')
+}
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ClerkProvider publishableKey={clerkPubKey}>
+      <App />
+    </ClerkProvider>
   </StrictMode>,
 )
